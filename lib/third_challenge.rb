8eac64ad49fb3c2fb5f918e1a4f518c55b91cf11
Robert Keller <robert_keller44@icloud.com>
2019-12-26 04:01:09 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -9,14 +9,13 @@ def third_challenge
         name: "Lady Montague", 
         age: "54"
         },
-      hero: {
+      :hero => {
        name: "Romeo",
        age: "15",
        status: "alive"
         },
       :hero_friends => []
    }, 
-   
    :capulet => {
       :patriarch => {
         name: "Lord Capulet",
